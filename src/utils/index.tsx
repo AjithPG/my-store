f375b4093a902f0b60d94990a1d47ba3ac04d347
ajithpg <ajithpg008@gmail.com>
2025-02-25 22:44:50 +0300

    feat:format currency

---
+++ src/utils/index.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const productionUrl = 'https://strapi-store-server.onrender.com/api';
+
+export const customFetch = axios.create({
+  baseURL: productionUrl,
+});
+
+
+
+export const formatPrice = (price:number):string => {
+    const dollarsAmount = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price / 100);
+    return dollarsAmount;
+  };
